f18f7bfbb391996c2d539f638c1de6a601a8b12a
Brilyan Satria Wahyuda <brilyansatria10@gmail.com>
2023-11-01 18:23:11 +0700

    added necessary classes for Tugas

---
+++ Polymorphism_Brilyan/src/Tugas/JumpingZombie.java
@@ -0,0 +1,31 @@
+package Tugas;
+
+public class JumpingZombie extends Zombie{
+    JumpingZombie(int health, int level){
+        this.health = health;
+        this.level = level;
+    }
+    @Override
+    public void heal(){
+        switch (this.level){
+            case 1:
+                this.health += (this.health * 0.3);
+                break;
+            case 2:
+                this.health += (this.health * 0.4);
+                break;
+            case 3:
+                this.health += (this.health * 0.5);
+                break;
+        }
+    }
+    public void destroyed(){
+        this.health -= (this.health * 0.01);
+    }
+    @Override
+    public String getZombieInfo(){
+        String info = "Jumping Zombie Data = \n";
+        info += super.getZombieInfo()+"\n";
+        return info;
+    }
+}
---
+++ Polymorphism_Brilyan/src/Tugas/Zombie.java
@@ -0,0 +1,17 @@
+package Tugas;
+
+public class Zombie implements Destroyable {
+    protected  int health;
+    protected  int level;
+
+    public  void heal(){
+
+    }
+    public void destroyed(){
+
+    }
+    public String getZombieInfo(){
+        return "";
+    }
+
+}
